fadd4a9cd4d20f5acb97158ab3cb943c9467fbdd
Rebecca Tong <rtong@buffalo.edu>
2018-07-25 18:10:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,8 @@ class ApplicationController < Sinatra::Base
     @recipe.cook_time = params[:cook_time]
     @recipe.save
     erb :show
+
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   delete '/recipes/:id/delete' do
