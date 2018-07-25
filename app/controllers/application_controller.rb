f65fa872ca436de9fd0d990fb56a09ee702ad04b
Rebecca Tong <rtong@buffalo.edu>
2018-07-25 18:07:52 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,6 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # get '/' do
-  #   erb :index
-  # end
 
   get '/recipes/new' do
     erb :new
@@ -14,18 +11,19 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
-    redirect "/recipes/#{@recipe.id}"
-  end
-
-  get '/recipes/:id' do
-    @recipe = Recipe.find_by_id(params[:id])
-    erb :show
+    redirect to "/recipes/#{@recipe.id}"
   end
 
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
+  
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
 
   get '/recipes/:id/edit' do
     @recipe = Recipe.find_by_id(params[:id])
