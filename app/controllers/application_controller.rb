2b28c475738342c55e88533de6c313cd276e49cc
Rebecca Tong <rtong@buffalo.edu>
2018-07-25 18:08:30 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < Sinatra::Base
 
 
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find_by_id(params[:id])
+    @recipe = Recipe.find(params[:id])
     erb :edit
   end
 
